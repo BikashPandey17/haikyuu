47eef2eedb48f5d25e99a9a5efa07c5f2e35748c
BikashPandey17 <bikash.pandey@springandriver.com>
2020-04-30 23:12:25 +0530

    Added the english version of chapter 391

--- scrap.py
+++ scrap.py
@@ -5,12 +5,14 @@ def main():
     sbo = SeleniumBrowser()
     browser = sbo.connect('https://haikyu-top.com/manga/haikyuu-chapter-391/')
 
+    # sleep for some time after opening the browser
+    time.sleep(5)
     SCROLL_PAUSE_TIME = 0.5
 
     i = 0
     for i in range(0,41):
         # Scroll down to bottom
-        browser.save_screenshot("data/screenshot{}.png".format(i))
+        browser.save_screenshot("data/english/screenshot{}.png".format(i))
         browser.execute_script("window.scrollTo(0, window.scrollY + 400)")
         # .format(1070*(i+1))
         # Wait to load page
